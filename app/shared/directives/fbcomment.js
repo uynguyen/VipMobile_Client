01096e99a6c8dfb59deb025443b3eb5e21a4a344
Lê Quốc Vin <winlqvin.it@gmail.com>
2016-01-06 19:39:30 +0700

    Fix sort comment

--- app/shared/directives/fbcomment.js
+++ app/shared/directives/fbcomment.js
@@ -1,10 +1,11 @@
 'use strict';
 
 appDirective.directive('dynFbCommentBox', function () {
-    function createHTML(href, numposts, datawidth) {
+    function createHTML(href, numposts, datawidth, orderby) {
         return '<div class="fb-comments" ' +
                        'data-href="' + href + '" ' +
                        'data-numposts="' + numposts + '" ' +
+                        'data-order-by="' + orderby + '" ' +
                        'data-width="' + datawidth + '">' +
                '</div>';
     }
@@ -16,9 +17,10 @@ appDirective.directive('dynFbCommentBox', function () {
             attrs.$observe('pageHref', function (newValue) {
                 var href        = newValue;
                 var numposts    = attrs.numposts    || 5;
+                var orderby    = attrs.orderby    || 'reverse_time';
                 var pagewidth = attrs.pagewidth || '100%';
 
-                elem.html(createHTML(href, numposts, pagewidth));
+                elem.html(createHTML(href, numposts, pagewidth, orderby));
                 FB.XFBML.parse(elem[0]);
             });
         }
